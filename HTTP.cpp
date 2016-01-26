1aba2dfaeafaec674e71596851b9d070c17bd6b3
Qingyang Chen <qingyang.chen@gmail.com>
2016-01-25 19:04:43 -0500

    added HTTP handling

--- HTTP.cpp
+++ HTTP.cpp
@@ -11,5 +11,18 @@ void HTTP::request(struct mbuf *data) {
   const struct mg_str& method = parsedRequest.method;
   if (strncmp(method.p, "POST", method.len) != 0) return;
 
+  // Parse the JSON.
+  const struct mg_str& body = parsedRequest.body;
+  struct json_token *json = parse_json2(body.p, body.len);
+  if (json == NULL) return;
+
   // Process different functions.
+  const struct mg_str& uri = parsedRequest.uri;
+  if (!strncmp("add_node", uri.p, uri.len)) {
+    printf("Got add_node: %.*s\n", (int)uri.len, uri.p);
+  } else if (!strncmp("add_edge", uri.p, uri.len)) {
+    printf("Got add_edge: %.*s\n", (int)uri.len, uri.p);
+  } else {
+    printf("Got bad URI: %.*s\n", (int)uri.len, uri.p);
+  }
 }
--- HTTP.h
+++ HTTP.h
@@ -3,4 +3,5 @@
 class HTTP {
 public:
   static void request(struct mbuf *data);
+
 };
