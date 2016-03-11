151c88f46f9ed63dd1c9d2284d8cac898d40bf2e
Qingyang Chen <qingyang.chen@gmail.com>
2016-03-11 10:59:44 -0500

    segfault

--- Log.BlockBuffer.cpp
+++ Log.BlockBuffer.cpp
@@ -6,11 +6,6 @@ uint32_t Log::BlockBuffer::id;
 void *Log::BlockBuffer::block;
 
 // This is for OS X.
-#ifndef aligned_alloc
-  void *aligned_alloc(int align, size_t size) {
-    return malloc(size);
-  }
-#endif
 
 void Log::BlockBuffer::init() {
   block = aligned_alloc(0x1000, BLOCK_SIZE);
